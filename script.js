546bfafb35daa20da12414d3e22e03b7e06a5afd
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-11-27 16:33:11 +0200

    dark mode styles

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ const image2 = document.getElementById("image2");
 const image3 = document.getElementById("image3");
 const textBox = document.getElementById("text-box");
 
+// Dark Mode Styles
+function darkMode() {
+  nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
+  textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
+  toggleIcon.children[0].textContent = "Dark Mode";
+  toggleIcon.children[1].classList.remove("fa-sun");
+  toggleIcon.children[1].classList.add("fa-moon");
+  image1.src = "img/undraw_proud_coder_dark.svg";
+  image1.src = "img/undraw_feeling_proud_dark.svg";
+  image1.src = "img/undraw_conceptual_idea_dark.svg";
+}
+
